b4b82e5b89dab10c595f337510bee398d4953642
Cillian Myles <myles.cillian@gmail.com>
2025-05-13 16:56:15 +0200

    chore: Update example

--- example/super_dates_example.dart
+++ example/super_dates_example.dart
@@ -4,9 +4,9 @@ void main() {
   final date = LocalDate(2023, 6, 29);
   print(date);
 
-  final dateTime = LocalDateTime(2023, 6, 29, 12, 34, 56, 789, 123);
-  print(dateTime);
+  final localDateTime = LocalDateTime(2023, 6, 29, 12, 34, 56, 789, 123);
+  print(localDateTime.toString());
 
   final utcDateTime = UtcDateTime(2023, 6, 29, 12, 34, 56, 789, 123);
-  print(utcDateTime);
+  print(utcDateTime.toIso8601String());
 }
